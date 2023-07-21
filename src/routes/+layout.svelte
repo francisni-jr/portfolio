4d009c11e39e02c26a5bbf3a52b50ab6810e25e1
* <francisnijr1.acc@gmail.com>
2023-07-21 16:47:22 -0300

    Adding responsivity in navigations, overflow hidden in body, more 1 page, and more...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,34 @@
 <script>
   import '../app.postcss';
-	import { ChevronDownIcon, HashIcon, Navigation2Icon, GithubIcon, InstagramIcon } from 'svelte-feather-icons'
-	
+	import { ChevronDownIcon, GridIcon, HashIcon, Navigation2Icon, GithubIcon, InstagramIcon } from 'svelte-feather-icons'
 	
 </script>
 
 
-
-<div id="app-lay" class="bg-is-main h-page font-general text-onmain break-words overflow-x-scroll overflow-y-scroll">
-  <header class="w-full flex items-start justify-center h-[20vh] bg-gradient-to-b from-is-main to-transparent sm:absolute md: text-onblock">
-    <div class="hidden">
-      <label class="btn btn-action" for="modal-2">About this software</label>
-      
-      <input class="modal-state" id="modal-2" type="checkbox" />
-      <div class="modal">
-        <label class="modal-overlay" for="modal-2"></label>
-        <div class="modal-content flex flex-col gap-5 max-w-3xl">
-          <label for="modal-2" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
-          <h2 class="text-xl">V0.0.0</h2>
-          <span>This software (portfolio), had the first version released in july/00/00. You can take a look at the
-            GitHub repository to analyze the source of the old versions. </span>
-          <div class="flex gap-3">
-            <button class="btn btn-error btn-block">Delete</button>
-            <button class="btn btn-block">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <nav class="popover z-30 md:px-0 md:ml-0 ml-10 px-10">
+<div id="app-lay" class="bg-gradient-to-b from-is-main via-is-main to-is-block h-page font-general text-onmain break-words overflow-x-scroll max-h-vh overflow-y-hidden
+">
+    <nav class="popover fixed z-40 top-0 left-0">
       <label
-        class="popover-trigger w-30 flex items-center justify-center rounded-b-lg bg-is-action text-onaction mb-5 h-20"
-        tabindex="0">
-        <ChevronDownIcon size="20" />
+        class="popover-trigger hover:shadow-[0_0_40px_yellow] w-25 h-25 flex items-start justify-start rounded-br-[100%] bg-is-action text-onaction mb-5 h-20"
+        tabIndex="50"
+        >
+        <GridIcon size="60" class="p-5" />
       </label>
-      <div class="popover-content md:mb-120 bg-is-block p-5" tabindex="0">
-        <div style="z-index: -1;" class="popover-arrow mt-5 p-5 bg-is-block"></div>
+      <div class="popover-content shadow-xl h-60 overflow-y-scroll mx-5 mt-30 absolute popover-right z-50 bg-is-block p-5 rounded-tl-lg">
         <div class="text-sm">
+          <p class="text-is-warn border p-5 rounded ">
+            <b class="">INFO</b>
+            <br />
+            <span>Click outside to close this menu</span>
+          </p>
           <ul translate="no" class="text-2xl p-5">
+            <p translate="yes" class="mx-auto text-onblock">PAGES | SCROLL DOWN</p>
+            <ChevronDownIcon class="mx-auto text-onblock mb-10" size="20" />
             <li style="word-wrap: break-word;" class="w-full rounded p-5 bg-is-action text-onaction">
-              <a class="" href="/">/</a>
+              <a class="" href="/#start">/</a>
             </li>
             <li style="word-wrap: break-word;" class="w-full rounded p-5 bg-is-action mt-5 text-onaction">
-              <a class="" href="#aboutme">/#aboutme</a>
+              <a class="" href="/#aboutme">/#aboutme</a>
             </li>
             <li style="word-wrap: break-word;" class="w-full rounded p-5 bg-is-action mt-5 text-onaction">
               <a class="" href="/#myexperience">/#myexperience</a>
@@ -53,68 +40,61 @@
               <a class="" href="/contactme">/contactme</a>
             </li>
           </ul>
-          <p class="text-is-warn border p-5 rounded ">
-            <b class="">INFO</b>
-            <br />
-            <span>Click outside to close this menu</span>
-          </p>
         </div>
       </div>
     </nav>
-  </header>
-  <aside class="fixed top-20 w-5 z-40 md:w-25 flex justify-center items-center md:h-full h-screen bg-gradient-to-t from-is-main to-transparent md:bg-transparent flex flex-col gap-5 px-10 md:px-25">
-    <p style="padding:10px;" class="fixed top-20 md:mx-20 mt-0 md:mt-10  right-0  md:py-5 md:w-25  md:bg-is-action flex justify-center items-center rounded-l-lg">
-      <a class="text-onaction" href="#top">
+  <aside class="fixed top-20 w-5 md:w-25 flex justify-center items-center md:h-full h-screen bg-transparent flex flex-col gap-5 px-10 md:px-25">
+    <p style="padding:8px;" class="">
+      <a class="text-onaction" href="/#start">
         <Navigation2Icon />
       </a>
     </p>
-    <span href="#" class=" text-sm">/</span>
     <ol translate="no" class="steps steps-vertical">
       <li class="step">
         <a href="/#welcome">
-          <div class="step-circle w-auto hover:shadow-md bg-is-action text-onaction">
+          <div class="step-circle w-auto hover:shadow-[0_0_40px_yellow] bg-is-action text-onaction">
             <HashIcon size="20" />
           </div>
         </a>
-        <span href="#" class=" text-sm">Welcome</span>
+        <span class=" text-sm">Welcome</span>
       </li>
       <li class="step">
-        <a href="#aboutme">
-          <div class="step-circle w-auto hover:shadow-md bg-is-action text-onaction ">
+        <a href="/#aboutme">
+          <div class="step-circle hover:shadow-[0_0_40px_yellow] w-auto bg-is-action text-onaction ">
             <HashIcon size="20" />
           </div>
         </a>
-        <span href="#" class=" text-sm">About Me</span>
+        <span class=" text-sm">About Me</span>
       </li>
       <li class="step">
-        <a href="#myexperience">
-          <div class="step-circle w-auto hover:shadow-md bg-is-action text-onaction">
+        <a href="/#myexperience">
+          <div class="step-circle w-auto hover:shadow-[0_0_40px_yellow] bg-is-action text-onaction">
             <HashIcon size="20" />
           </div>
         </a>
-        <span href="#" class="text-sm text-center">My <br class="md:hidden" /> Experience</span>
+        <span class="text-sm text-center">My <br class="md:hidden" /> Experience</span>
       </li>
       <li class="step">
-        <a href="#">
-          <div class="step-circle w-auto hover:shadow-md bg-is-action text-onaction">
+        <a href="/#aboutportfolio">
+          <div class="step-circle w-auto hover:shadow-[0_0_40px_yellow] bg-is-action text-onaction">
             <HashIcon size="20" />
           </div>
         </a>
-        <span href="#" class="text-sm text-center">About <br class="md:hidden"/> this project</span>
+        <span class="text-sm text-center">About <br class="md:hidden"/> this project</span>
       </li>
     </ol>
   </aside>
   <slot />
-  <footer class="p-5 bg-transparent fixed bottom-0 right-0 flex flex-col justify-center items-center gap-5 border md:w-25 w-20 mx-5 md:mx-20 border-yellow ">
-    <a class="text-onaction" href="#">
+  <footer class="p-5 bg-is-main fixed bottom-0 right-0 flex flex-col justify-center items-center gap-5 border md:w-25 w-20 mx-5 md:mx-20 border-yellow ">
+    <a class="text-onaction" href="https://github.com/francisni-jr">
       <GithubIcon size="30" class="" />
     </a>
-    <a class="text-onaction" href="#">
+    <a class="text-onaction" href="https://instagram.com/">
       <InstagramIcon size="30" class="" />
     </a>
     <p class="flex justify-center items-center">
-      <details class="flex flex-col justify-center items-center" >
-        <summary>
+      <details translate="no" class="flex flex-col justify-center items-center" >
+        <summary class="sm:hidden">
           &copy
         </summary>
         <div class="flex flex-col justify-center items-center my-5">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script type="text/javascript" charset="utf-8">
   import HomeLoader from '../loaders/HomeLoader.svelte'
+  import { PlayIcon } from 'svelte-feather-icons'
 </script>
 
 <!--<HomeLoader  />-->
 
-<main class="bg-is-main mt-[10vh] md:mt-0 md:px-60 ml-10 md:ml-0 px-10">
-  <section
-    id="top"
-    class="h-[100vh] md:mt-60 mt-10 scroll-mt-10 text-onmain mx-auto flex flex-col rounded-t-lg mb-10">
-    <div class="p-5 mx-auto font-header text-center">
+<main class="md:mt-0 md:px-60 ml-10 md:ml-0 px-10">
+  <section id="start" class="h-vh mt-10 max-h-[90vh] scroll-mt-10 text-onmain mx-auto flex flex-col rounded-t-lg mb-10">
+    <div class="p-5 z-20 mx-auto font-header text-center">
       <h1>
         <span class="text-4xl">This is a</span>
         <div class=""></div>
@@ -18,77 +17,105 @@
         <div class="pt-10"></div>
         <span class="text-4xl">Portfolio</span>
       </h1>
-      <div class="divider pt-10"></div>
-      <h2 class="text-1xl pt-10">
+      
+      <div class="divider flex items-end justify-end pt-10">
+        
+      </div>
+      
+      <h2 class="text-1xl mt-10">
         Browse the website to get to know it
       </h2>
     </div>
   </section>
   <section id="welcome"
-    class="scroll-mt-10 mt-auto bg-gradient-to-b from-is-block to-transparent text-onblock w-30 mx-auto w-full flex flex-col h-vh rounded-t-lg text-center">
+    class="shadow-[0_0_10px_black] scroll-mt-10 mt-10 bg-is-block md:bg-gradient-to-b from-is-block to-transparent text-onblock w-30 mx-auto w-full flex flex-col h-[100vh] rounded-t-lg text-center">
     <h2 class="px-20 pt-20 p-5 font-header text-4xl">Hi👋🏼, Welcome to my <span
         class="font-highlight font-bold text-2xl">PORTFOLIO</span></h2>
     <h3 class="px-20 pb-20">
       This project is aimed at anyone who wants to know more <a class="text-onaction" href="#">about me</a> ( Francisní
-      Jr ) or who wants to get involved in this or other <a class="text-onaction" href="#">projects</a> I have created.
-      The header menu will be hidden to give you a better experience.
+      Jr ) or who wants to get involved in this or other <a class="text-onaction" href="/projects">projects</a> I have
+      created.
     </h3>
+    
   </section>
   <section id="aboutme"
-    class="scroll-mt-10 mt-auto bg-gradient-to-b from-is-block to-transparent text-onblock h-auto w-30 mx-auto w-full flex flex-col h-vh rounded-t-lg text-center">
+    class="shadow-[0_0_10px_black] scroll-mt-10 mt-10 bg-is-block md:bg-gradient-to-b from-is-block to-transparent text-onblock w-30 mx-auto w-full flex flex-col h-[100vh] rounded-t-lg text-center">
     <h2 class="px-20 pt-20 p-5 font-header text-3xl">I'm
       <span
         class="font-highlight underline text-3xl text-onblock drop-shadow-lg animate-pulse duration-1000 delay-1000">Francisní</span>
       ,
       <br class="md:hidden" />
-      <span class="font-header text-3xl">A software developer who loves what do</span>
+      <span class="font-header text-3xl">A software developer from last line</span>
     </h2>
     
-    <div class="flex flex-wrap justify-center items-center mx-auto gap-5">
+    <div class="flex flex-wrap justify-center items-center mx-auto gap-5 overflow-y-scroll">
       <div class="">
-        <p class="p-5 mt-10 text-3xl">Picture</p>
+        <p class="p-5 mt-5 text-3xl">Picture</p>
         <div class="avatar mx-auto h-30  w-30">
           <img src="https://res.cloudinary.com/dt2se4cle/image/upload/v1689804230/20230602_140608_3_lmcuky.jpg"
             alt="avatar" />
         </div>
       </div>
-            <div class="">
-        <p class="p-5 mt-10 text-3xl">Brand</p>
+      <div class="">
+        <p class="p-5 mt-5 text-3xl">Brand</p>
         <div class="avatar p-5 mx-auto h-30  w-30">
-          <img src="https://res.cloudinary.com/dt2se4cle/image/upload/v1689629217/Brand/brand_symbolic_francisnijr_gfpve6.png"
+          <img
+            src="https://res.cloudinary.com/dt2se4cle/image/upload/v1689629217/Brand/brand_symbolic_francisnijr_gfpve6.png"
             alt="avatar" />
         </div>
       </div>
+      <h3 class="px-20 py-20 pb-20">
+        You can see my brand used like profile in social media or favicon in websites. It represents me and my work, So
+        maybemaybe you see me around in a photo or in a logo, stay tuned.
+      </h3>
     </div>
-        <h3 class="px-20 py-20 pb-20 overflow-y-scroll">
-      You can see my brand used like profile in social media or favicon in websites. It represents me and my work, So maybemaybe you see me around in a photo or in a logo, stay tuned. 
-    </h3>
+    
   </section>
   <section id="myexperience"
-    class="scroll-mt-10 mt-10 bg-gradient-to-b from-is-block to-transparent text-onblock h-auto w-30 mx-auto w-full flex flex-col h-vh rounded-t-lg text-center">
-    <h2 class="px-20 pt-20 p-5 font-header text-3xl">
-      My specialty is the web and I have experience in other areas as well. But here I will touch only on what I am better.
-    </h2>
-    
-    <div class="flex flex-wrap justify-center items-center mx-auto gap-5">
-      <div class="">
-        <p class="p-5 mt-10 text-3xl">Picture</p>
-        <div class="avatar mx-auto h-30  w-30">
-          <img src="https://res.cloudinary.com/dt2se4cle/image/upload/v1689804230/20230602_140608_3_lmcuky.jpg"
-            alt="avatar" />
-        </div>
-      </div>
-            <div class="">
-        <p class="p-5 mt-10 text-3xl">Brand</p>
-        <div class="avatar p-5 mx-auto h-30  w-30">
-          <img src="https://res.cloudinary.com/dt2se4cle/image/upload/v1689629217/Brand/brand_symbolic_francisnijr_gfpve6.png"
-            alt="avatar" />
-        </div>
+    class="shadow-[0_0_10px_black] scroll-mt-10 mt-10 bg-is-block md:bg-gradient-to-b from-is-block to-transparent text-onblock w-30 mx-auto w-full flex flex-col h-[100vh] rounded-t-lg text-center">
+    <p class="px-20 pt-20 p-5 ">
+      My specialty is the web and I have experience in other areas as well. But here I will touch only on what I am
+      better.
+    </p>
+    <h3 class="p-5 px-20 text-3xl overflow-y-scroll sm:hidden">My main tools and languages I've worked on so far:</h3>
+    <div class="px-20 text-3xl sm:hidden text-header md:py-5">
+      <div class="flex justify-center sm:hidden items-center md:visible flex-nowrap w-screen overflow-x-scroll">
+        
+        <i class="devicon-html5-plain">
+          <span class="px-5">HTML</span>
+        </i>
+        
+        <i class="devicon-css3-plain">
+          <span class="px-5">CSS</span>
+        </i>
+        
+        
+        <i class="devicon-javascript-plain">
+          <span class="px-5">JAVASCRIPT</span>
+        </i>
+        
+        
+        <i class="devicon-svelte-plain">
+          <span class="px-5">SVELTEJS</span>
+        </i>
+        
+        <i class="devicon-vuejs-plain">
+          <span class="px-5">VUEJS</span>
+        </i>
+        
+        <i class="devicon-nodejs-plain">
+          <span class="px-5">NODEJS</span>
+        </i>
+        
       </div>
     </div>
-        <h3 class="px-20 py-20 pb-20 overflow-y-scroll">
-      You can see my brand used like profile in social media or favicon in websites. It represents me and my work, So maybemaybe you see me around in a photo or in a logo, stay tuned. 
-    </h3>
+    <h3 class="p-5 mx-20 font-header text-3xl">What I do?</h3>
+    <div class="px-20 overflow-y-scroll">
+      <p class="">At the moment I'm just doing individual hands-on projects, or helping other devs build their tools. I
+        don't have a permanent job, my income is varied and my time is versatile. I am looking to evolve more into the
+        mobile or hybrid development environment, but the web is my home and I will not stop applying it.
+      </p>
+    </div>
   </section>
 </main>
 
